611e90977c473ce774fd42ec6e0741b569c2f45f
Daniel Wilches <dwilches@proton.me>
2026-07-22 22:50:18 -0400

    Add welcome dialog

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
 import type { Route } from "./+types/home";
+import WelcomeDialog from "~/dialogs/welcome-dialog";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "React Game Collection" },
-    { name: "description", content: "Some simple games using React" },
-  ];
+    return [
+        { title: "React Game Collection" },
+        { name: "description", content: "Some simple games using React" },
+    ];
 }
 
+const WelcomeDialogAlreadyShown = "WelcomeDialogAlreadyShown";
+
 export default function Home() {
-  return (<>
+
+    const dialogAlreadyShown = JSON.parse(localStorage.getItem(WelcomeDialogAlreadyShown) ?? "false");
+    const [isOpen, setIsOpen] = useState<boolean>(!dialogAlreadyShown);
+
+    const closeDialog = () => {
+        setIsOpen(false);
+        localStorage.setItem(WelcomeDialogAlreadyShown, "true");
+    };
+
+    return (<>
+        <WelcomeDialog isOpen={ isOpen } onClose={ closeDialog }/>
     </>);
 }
