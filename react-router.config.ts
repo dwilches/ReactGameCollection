a85a9451ca4619afe3044b569e8a21143dce65bf
Daniel Wilches <dwilches@proton.me>
2026-07-24 11:30:44 -0400

    Enable GH Pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
+const localBuild = process.env.NODE_ENV === "development";
+
 export default {
   ssr: false,
+
+  // Allows serving the SPA from GH Pages
+  basename: localBuild ? "/" :  "/ReactGameCollection/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+const localBuild = process.env.NODE_ENV === "development";
+
 export default defineConfig({
+  // Allows serving the SPA from GH Pages
+  base: localBuild ? "/" : "/ReactGameCollection/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
